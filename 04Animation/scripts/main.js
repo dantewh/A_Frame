3e30448a159bf9197d4a7597cf01071b8dd1c63d
dantewh <61277342+dantewh@users.noreply.github.com>
2020-05-21 15:07:57 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,10 +1,12 @@
-// changing individual properties with code and using setInterval
-var rotationSpeed = 0.02;
-var myOtherBox = document.getElementById('myOtherBox');
 
-function spin(){
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.y);
-}
 
-setInterval(spin, 16); //equivalent to 60 fps
+ // changing individual properties with code and using setInterval
+ var rotationSpeed = 0.01;
+ var myOtherBox = document.getElementById('myOtherBox');
+
+ function spin(){
+ 	myOtherBox.object3D.rotation.y += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.y);
+ }
+
+ setInterval(spin, 16); //equivalent to 60 fps
